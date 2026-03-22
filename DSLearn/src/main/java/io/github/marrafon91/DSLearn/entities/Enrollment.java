d16409d80637ff4d3b30e6de9af189f3442f4fe0
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-03-22 07:13:47 -0300

    Enrollment, EnrollmentPK

---
+++ DSLearn/src/main/java/io/github/marrafon91/DSLearn/entities/Enrollment.java
@@ -0,0 +1,84 @@
+package io.github.marrafon91.DSLearn.entities;
+
+import io.github.marrafon91.DSLearn.entities.pk.EnrollmentPK;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private Instant enrollmentMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private Instant refundMoment;
+
+    private boolean available;
+    private boolean onlyUpdate;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(User user, Offer offer, Instant enrollmentMoment, Instant refundMoment, boolean available, boolean onlyUpdate) {
+        id.setUser(user);
+        id.setOffer(offer);
+        this.enrollmentMoment = enrollmentMoment;
+        this.refundMoment = refundMoment;
+        this.available = available;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User user) {
+        id.setUser(user);
+    }
+
+    public Offer getOffer() {
+        return id.getOffer();
+    }
+
+    public void setOffer(Offer offer) {
+        id.setOffer(offer);
+    }
+
+    public Instant getEnrollmentMoment() {
+        return enrollmentMoment;
+    }
+
+    public void setEnrollmentMoment(Instant enrollmentMoment) {
+        this.enrollmentMoment = enrollmentMoment;
+    }
+
+    public Instant getRefundMoment() {
+        return refundMoment;
+    }
+
+    public void setRefundMoment(Instant refundMoment) {
+        this.refundMoment = refundMoment;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isOnlyUpdate() {
+        return onlyUpdate;
+    }
+
+    public void setOnlyUpdate(boolean onlyUpdate) {
+        this.onlyUpdate = onlyUpdate;
+    }
+
+}
