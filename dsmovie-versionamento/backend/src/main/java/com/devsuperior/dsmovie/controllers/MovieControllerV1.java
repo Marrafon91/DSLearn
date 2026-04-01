7aa146eada40491999d6d8c4202ff765eb2bd59d
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-01 08:16:51 -0300

    Versionamento via URI

---
+++ dsmovie-versionamento/backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieControllerV1.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dsmovie.controllers;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.MovieGenreDTO;
+import com.devsuperior.dsmovie.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/movies")
+public class MovieControllerV1 {
+
+	@Autowired
+	private MovieService service;
+	
+	@GetMapping
+	public Page<MovieGenreDTO> findAll(Pageable pageable) {
+		return service.findAllWithGenre(pageable);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public MovieGenreDTO findById(@PathVariable Long id) {
+		return service.findByIdMovieGenre(id);
+	}
+}
