5b4d48cb0ad2de7317627714a605c299c532f892
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-07 15:34:43 -0300

    GET user by id

--- mongoDB/src/main/java/io/github/marrafon91/mongoDB/services/UserService.java
+++ mongoDB/src/main/java/io/github/marrafon91/mongoDB/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -22,5 +23,11 @@ public class UserService {
             throw new ResourceNotFoundException("Usúario não encontrado! ");
         }
         return result.stream().map(UserDTO::new).toList();
+    }
+
+    public UserDTO findById(String id) {
+        Optional<User> result = userRepository.findById(id);
+        return result.map(UserDTO::new)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado. ID: " + id));
     }
 }
