1333f003c27b4e0395a2a9b326ec850beec9235d
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-13 10:32:11 -0300

    Buscar posts usando vários critérios

--- spring-mongodb-ref-main/spring-boot-3-4-5/src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ spring-mongodb-ref-main/spring-boot-3-4-5/src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -10,6 +10,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.time.Instant;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -31,5 +33,18 @@ public class PostController {
         text = URL.decodeParam(text);
 
         return service.findByTitle(text);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public Flux<PostDTO> fullSearch(
+            @RequestParam(value = "text", defaultValue = "")String text,
+            @RequestParam(value = "minDate", defaultValue = "")String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "")String maxDate) throws UnsupportedEncodingException, ParseException {
+
+        text = URL.decodeParam(text);
+        Instant min = URL.convertDate(minDate, Instant.EPOCH);
+        Instant max = URL.convertDate(maxDate, Instant.now());
+
+        return service.fullSearch(text, min, max);
     }
 }
