59751b8bb93554fad69a3472e487016bb2cd4c28
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-13 07:36:18 -0300

    Configuração do mongodb

--- mongoDB-web-flux/src/main/java/io/github/marrafon91/mongoDB/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.marrafon91.mongoDB.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- mongoDB-web-flux/src/main/java/io/github/marrafon91/mongoDB/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.marrafon91.mongoDB.repositories;
-
-import io.github.marrafon91.mongoDB.entities.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
