4752937dc3108c75aa1bdff4dccf04eec6660f0d
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-17 07:19:32 -0300

    Projeto criado, job and step

---
+++ helloWorldSpringBatch/src/test/java/io/github/marrafon91/helloWorldSpringBatch/HelloWorldSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.helloWorldSpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
