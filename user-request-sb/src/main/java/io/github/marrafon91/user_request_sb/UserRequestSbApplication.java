809d8fe60367e55ae32f85fd47f2e4ca55391b45
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-23 14:24:08 -0300

    Projeto criado

---
+++ user-request-sb/src/main/java/io/github/marrafon91/user_request_sb/UserRequestSbApplication.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.user_request_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRequestSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRequestSbApplication.class, args);
+	}
+
+}
---
+++ user-request-sb/src/test/java/io/github/marrafon91/user_request_sb/UserRequestSbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.user_request_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
