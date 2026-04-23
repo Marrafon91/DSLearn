d3f2a9287fdb9caf15d43f551c3005a5e0599dbd
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-23 16:31:10 -0300

    Projeto clonado

---
+++ user-request-sb/src/main/java/io/github/marrafon91/user_request_sb/Writer/InsertUserDataDBWriterConfig.java
@@ -0,0 +1,15 @@
+package io.github.marrafon91.user_request_sb.Writer;
+
+import io.github.marrafon91.user_request_sb.dto.UserDTO;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InsertUserDataDBWriterConfig {
+
+    @Bean
+    public ItemWriter<UserDTO> insertUserDataDBWriter() {
+        return users -> users.forEach((System.out::println));
+    }
+}
