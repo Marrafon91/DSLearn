fdcb57e3167b9a5039c3bda2dbcbcd569cb0f042
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-25 07:07:26 -0300

    Processamento Parte 2

--- user-request-sb/src/main/java/io/github/marrafon91/user_request_sb/Writer/InsertUserDataDBWriterConfig.java
+++ user-request-sb/src/main/java/io/github/marrafon91/user_request_sb/Writer/InsertUserDataDBWriterConfig.java
@@ -1,6 +1,6 @@
 package io.github.marrafon91.user_request_sb.Writer;
 
-import io.github.marrafon91.user_request_sb.dto.UserDTO;
+import io.github.marrafon91.user_request_sb.entities.User;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class InsertUserDataDBWriterConfig {
 
     @Bean
-    public ItemWriter<UserDTO> insertUserDataDBWriter() {
+    public ItemWriter<User> insertUserDataDBWriter() {
         return users -> users.forEach((System.out::println));
     }
 }
--- user-request-sb/src/main/java/io/github/marrafon91/user_request_sb/processor/SelectFiledsUserDataProcessorConfig.java
+++ user-request-sb/src/main/java/io/github/marrafon91/user_request_sb/processor/SelectFiledsUserDataProcessorConfig.java
@@ -5,7 +5,6 @@ import io.github.marrafon91.user_request_sb.entities.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +13,23 @@ public class SelectFiledsUserDataProcessorConfig {
 
     private static Logger logger = LoggerFactory.getLogger(SelectFiledsUserDataProcessorConfig.class);
 
+    private int counter = 0;
+
     @Bean
     public ItemProcessor<UserDTO, User> selectFiledsUserDataProcessor() {
-        
+        return new ItemProcessor<UserDTO, User>() {
+
+            @Override
+            public User process(UserDTO item) throws Exception {
+                User user = new User();
+                user.setLogin(item.getLogin());
+                user.setName(item.getName());
+                user.setAvatarUrl(item.getAvatarUrl());
+                counter++;
+                logger.info("[PROCESSOR STEP] select user fields {} - {}", counter, user);
+                return user;
+
+            }
+        };
     }
 }
