54180fa532f70a1ee212c1a6f6447b411e9699dc
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-04-26 20:01:36 -0300

    Etapa processamento - preparando email a ser enviado

--- send-book-email-sb/src/main/java/io/github/marrafon91/send_book_email_sb/step/SendEmailUserStepConfig.java
+++ send-book-email-sb/src/main/java/io/github/marrafon91/send_book_email_sb/step/SendEmailUserStepConfig.java
@@ -1,9 +1,11 @@
 package io.github.marrafon91.send_book_email_sb.step;
 
+import com.sendgrid.helpers.mail.Mail;
 import io.github.marrafon91.send_book_email_sb.domain.UserBookLoan;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +23,16 @@ public class SendEmailUserStepConfig {
 
     @Bean
     public Step sendEmailUserStep(ItemReader<UserBookLoan> readUserWhithLoansCloseToReturnReader,
+                                  ItemProcessor<UserBookLoan, Mail> processLoanNotificationEmailProcessor,
                                   ItemWriter<UserBookLoan> sendEmailRequestReturnWriter,
                                   JobRepository jobRepository) {
 
         return new StepBuilder("sendEmailUserStep", jobRepository)
-                .<UserBookLoan, UserBookLoan>chunk(1, transactionManager)
+                .<UserBookLoan, Mail>chunk(1, transactionManager)
                 .reader(readUserWhithLoansCloseToReturnReader)
+                .processor(processLoanNotificationEmailProcessor)
                 .writer(sendEmailRequestReturnWriter)
+
 
                 .build();
     }
